71716b8f885b7c438a0253ea84051a26e491266f
kasiveyser <kasiveyser@gmail.com>
2016-06-26 15:57:40 +0300

    reverse

---
+++ eloquent_javascript/reverseArray.js
@@ -0,0 +1,36 @@
+// Обращаем вспять массив
+
+// У массивов есть метод reverse, меняющий порядок 
+// элементов в массиве на обратный. В качестве 
+// упражнения напишите две функции, reverseArray 
+// и reverseArrayInPlace. Первая получает массив 
+// как аргумент и выдаёт новый массив – с обратным 
+// порядком элементов. Вторая работает как 
+// оригинальный метод reverse – она меняет 
+// порядок элементов на обратный в том массиве, 
+// который был ей передан в качестве аргумента. 
+// Не используйте стандартный метод reverse.
+
+function reverseArray(arr) {
+	var x = new Array;
+	for (i = arr.length-1; i >= 0; i--){
+		x.push(arr[i]);
+	}
+	return x;
+}
+
+function reverseArrayInPlace(arr){
+	var arrHelper = new Array;
+
+	for(i = arr.length-1; i >= 0; i--){
+		arrHelper.push(arr[i]);
+	}
+
+	for(i = 0; i <= arrHelper.length-1; i++){
+		// arr.splice(i, 1, (arrHelper[i]));
+		arr[i] = arrHelper[i];
+	}
+	return arr;
+}
+
+reverseArrayInPlace([1, 2, 3, 7, 8, 9]);
